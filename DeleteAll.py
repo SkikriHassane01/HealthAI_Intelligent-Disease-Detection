c360238fd56b94ce0a3705f810e291b058decb86
HASSANE SKIKRI <hassaneskikri@gmail.com>
2024-09-10 00:47:40 +0100

    update the requirements

--- DeleteAll.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-import sys
-with open('requirements.txt') as f:
-    packages = f.read().splitlines()
-
-subprocess.check_call([sys.executable, '-m', 'pip', 'uninstall', '-y'] + packages)
--- delete.py
+++ delete.py
@@ -1,7 +1,19 @@
 import subprocess
-
 import sys
-with open('requirements.txt') as f:
-    packages = f.read().splitlines()
+import chardet
+
+with open('requirements.txt', 'rb') as f:
+    raw_data = f.read()
+    result = chardet.detect(raw_data)
+    encoding = result['encoding']
+    print(f"Detected encoding: {encoding}")
+
+try:
+    cleaned_content = raw_data.decode(encoding).replace('\x00', '')
+except UnicodeDecodeError as e:
+    print(f"Error decoding with {encoding}: {e}")
+    sys.exit(1)
+
+packages = cleaned_content.splitlines()
 
 subprocess.check_call([sys.executable, '-m', 'pip', 'uninstall', '-y'] + packages)
